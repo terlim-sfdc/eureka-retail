4e6b9f162c6381edd2b3403a5cd9ee426da9f6ea
Terence Lim <terence.lim@salesforce.com>
2022-03-02 13:20:29 +0800

    Updated about screen with blog link and slack links

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -7,10 +7,14 @@ import {
   ScrollView,
   TouchableOpacity,
   Linking,
+  Image,
 } from "react-native";
 import AppLoading from "expo-app-loading";
 import colors from "../../assets/colors/colors";
 import { useFonts } from "expo-font";
+import { Button } from "react-native-paper";
+
+import slackLogo from "../../assets/images/slack-logo.png";
 
 // Import components and styles
 import { container, headerWithoutSearch, headerContainer } from "../styles";
@@ -51,12 +55,24 @@ const AboutScreen = ({ route, navigation }) => {
 
         {/* Content Body */}
         <View style={styles.infoBackground}>
-          <Text style={styles.title}>About This App</Text>
+          <Text style={styles.title}>About Eureka Retail</Text>
           <Text style={styles.content}>
-            This app is built using React Native, showcasing the flexibility of
-            using pro-code and open source tools such as Javascript to build a
-            native mobile app.
+            This app (Eureka Retail) is built using React Native, showcasing the
+            flexibility of using pro-code and open source tools such as
+            Javascript to build a custom native mobile app, combining the power
+            of the Salesforce Platform and multi-cloud to create an amazing
+            Customer 360 experience.
           </Text>
+
+          <Button
+            icon="eye"
+            mode="contained"
+            onPress={() => Linking.openURL("https://sforce.co/3M8hfvl")}
+            marginBottom={10}
+          >
+            Eureka Retail Behind the Scenes
+          </Button>
+
           <Text style={styles.title}>Customer Data</Text>
           <Text style={styles.content}>
             Customer data interfaced within this app is directly retrieved from
@@ -65,36 +81,144 @@ const AboutScreen = ({ route, navigation }) => {
             is synchronized with the Salesforce object in the Salesforce
             customer (demo) tenant using Heroku Connect.
           </Text>
-          <Text style={styles.title}>Heroku Connect</Text>
-          <Text style={styles.content}>
-            Heroku Connect is an add-on that synchronizes data between a
-            Salesforce organization and a Heroku Postgres database. Using Heroku
-            Connect with Heroku Postgres, developers can build custom
-            applications (such as this mobile app) that interacts with
-            Salesforce data easily, securely and seamlessly.
-          </Text>
-          <Text style={styles.title}>Express Node.js</Text>
-          <Text style={styles.content}>
-            Express is a minimal and flexible Node.js web application framework
-            that provides a robust set of features for web and mobile
-            applications, which makes creating a robust API quick and easy.
-          </Text>
-          <Text style={styles.title}>Eureka Team</Text>
-          <Text style={styles.content}>
-            {"\u2022"} Terence Lim (Developer Evangelist) {"\n"}
-            {"\u2022"} Ji Soo Kim (UX Designer)
-          </Text>
-          <Text style={styles.title}>
-            Full Details on Implementation and Source Codes:
-          </Text>
+
+          <Text style={styles.title}>Connect with the Eureka Team</Text>
+
+          {/* Eureka Mobile Request Channel */}
           <TouchableOpacity
             onPress={() =>
-              Linking.openURL(
-                "https://milenology.com/blog/reactnativesalesforce"
-              )
+              Linking.openURL("slack://channel?team=T01GST6QY0G&id=C033D7XT4RG")
             }
           >
-            <Text style={styles.links}>{"\u2022"}Eureka Mobile App Blog</Text>
+            <View style={styles.slackButton}>
+              <View>
+                <Text style={{ fontWeight: "bold" }}>
+                  #eureka-mobile-request
+                </Text>
+                <Text>Join our slack channel!</Text>
+              </View>
+              <View
+                style={{
+                  padding: 3,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={slackLogo}
+                  style={{ height: 20, width: 20, marginHorizontal: 5 }}
+                ></Image>
+                <Text>Slack Channel</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          {/* Terence User ID */}
+          <TouchableOpacity
+            onPress={() =>
+              Linking.openURL("slack://user?team=T01GST6QY0G&id=U0275D3JAKD")
+            }
+          >
+            <View style={styles.slackButton}>
+              <View>
+                <Text style={{ fontWeight: "bold" }}>Terence Lim</Text>
+                <Text>Developer Evangelist</Text>
+              </View>
+              <View
+                style={{
+                  padding: 3,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={slackLogo}
+                  style={{ height: 20, width: 20, marginHorizontal: 5 }}
+                ></Image>
+                <Text>Slack Message</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          {/* Jisoo User ID */}
+          <TouchableOpacity
+            onPress={() =>
+              Linking.openURL("slack://user?team=T01GST6QY0G&id=U02DFM4NWHL")
+            }
+          >
+            <View style={styles.slackButton}>
+              <View>
+                <Text style={{ fontWeight: "bold" }}>Ji Soo Kim</Text>
+                <Text>Senior Visual Designer</Text>
+              </View>
+              <View
+                style={{
+                  padding: 3,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={slackLogo}
+                  style={{ height: 20, width: 20, marginHorizontal: 5 }}
+                ></Image>
+                <Text>Slack Message</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          {/* Ian Douglas User ID */}
+          <TouchableOpacity
+            onPress={() =>
+              Linking.openURL("slack://user?team=T01GST6QY0G&id=U01GM42APC1")
+            }
+          >
+            <View style={styles.slackButton}>
+              <View>
+                <Text style={{ fontWeight: "bold" }}>Ian Douglas</Text>
+                <Text>Director</Text>
+              </View>
+              <View
+                style={{
+                  padding: 3,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={slackLogo}
+                  style={{ height: 20, width: 20, marginHorizontal: 5 }}
+                ></Image>
+                <Text>Slack Message</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          {/* Vivek User ID */}
+          <TouchableOpacity
+            onPress={() =>
+              Linking.openURL("slack://user?team=T01GST6QY0G&id=U01FT7G6NKZ")
+            }
+          >
+            <View style={styles.slackButton}>
+              <View>
+                <Text style={{ fontWeight: "bold" }}>Vivek Mahapatra</Text>
+                <Text>Senior Director</Text>
+              </View>
+              <View
+                style={{
+                  padding: 3,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  source={slackLogo}
+                  style={{ height: 20, width: 20, marginHorizontal: 5 }}
+                ></Image>
+                <Text>Slack Message</Text>
+              </View>
+            </View>
           </TouchableOpacity>
         </View>
       </ScrollView>
@@ -115,7 +239,17 @@ const styles = StyleSheet.create({
   },
   links: {
     fontSize: 15,
-    fontWeight: "bold",
+  },
+  slackButton: {
+    borderWidth: 1,
+    borderRadius: 15,
+    height: 56,
+    borderColor: colors.gray,
+    padding: 10,
+    margin: 5,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
 
